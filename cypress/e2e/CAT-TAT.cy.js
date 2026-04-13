3e41863e4d975de4bf46aea1fab02c54a06c8d63
leocostatmt <leonardocaetanocosta2005@gmail.com>
2026-04-13 14:19:18 -0300

    Teste Commit

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('passes 2' , () => {
+    cy.visit('https://linkedin.com')
+  })
 })
