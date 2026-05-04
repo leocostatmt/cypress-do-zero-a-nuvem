200515f0422e074e1b5c7c25396c54dc91bf1972
leocostatmt <leonardocaetanocosta2005@gmail.com>
2026-05-04 17:22:01 -0300

    modified:   cypress/e2e/CAT-TAT.cy.js

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -1,11 +1,27 @@
 describe('CEntral de Atendimento ao Cliente', () => {
-  it.only('acessa a aplicação' , () => {
+
+
+  beforeEach('acessa a aplicação', () => {
     cy.visit('src/index.html')
-  })
+  }
+)
+
 
   it('verifica o titulo da aplicação' , () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('joao.silva@example.com')
+    cy.get('#open-text-area').type('Gostaria de mais informações sobre o serviço.')
+    cy.get('button[type="submit"]').click()
+  })
+
+  it('exibe mensagem de erro ao enviar o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('button[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+  })
 
 })
