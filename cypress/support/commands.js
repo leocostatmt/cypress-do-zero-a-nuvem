c4906ff92473d4944517bff5b817c2dfce715e65
leocostatmt <leonardocaetanocosta2005@gmail.com>
2026-05-12 18:56:32 -0300

    modified:   cypress/support/commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,9 @@
 
- Cypress.Commands.add('login', (email, senha) => { })
+ Cypress.Commands.add('login', (email, senha) => { 
+    cy.get('#email').type(email)
+    cy.get('#password').type(senha)
+    cy.get('button[type="submit"]').click()
+ })
 
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
