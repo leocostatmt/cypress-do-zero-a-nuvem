87f82e36260b8cc6072f24ae06f4e8097c3ac66a
leocostatmt <leonardocaetanocosta2005@gmail.com>
2026-05-14 15:02:33 -0300

    modified:   cypress/support/commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,6 +5,36 @@
     cy.get('button[type="submit"]').click()
  })
 
+   Cypress.Commands.add('loginVisualStore', (usuario, senha) => {
+    cy.get('#usuario').type(usuario)
+    cy.get('#senha').type(senha)
+    cy.get('button[type="submit"]').click()
+ })
+
+ Cypress.Commands.add('logout', () => {
+    cy.get('button[class="m0l0iout_nochild" ] , [ id="e0_305i"]').click()
+ })
+
+ Cypress.Commands.add('limparCampos', () => {
+    cy.get('button[type="button" ] , [id="btnLimpar"]').click()
+ })
+
+ Cypress.Commands.add('verificarMensagem', (mensagem) => {
+    cy.get('#lblMensagem').should('be.visible').and('contain', mensagem)
+ })
+
+ Cypress.Commands.add('verificarUrl', (url) => {
+    cy.url().should('include', url)
+ })
+
+ Cypress.Commands.add('verificarCampoVazio', (campo) => {
+    cy.get(campo).should('have.value', '')
+ })
+
+ Cypress.Commands.add('verificarElementoVisivel', (seletor) => {
+    cy.get(seletor).should('be.visible')
+ })
+
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
